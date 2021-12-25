514af5f04d58f8d1a6281818fdd6d603d2ab9949
Robert Bartlensky <bartlensky.robert@gmail.com>
2021-12-25 00:44:55 +0000

    Fix tests.

--- tests/same_objdump.rs
+++ tests/same_objdump.rs
@@ -1,15 +1,20 @@
 use dld;
 use tempfile::NamedTempFile;
 
-use std::{path::PathBuf, process::Command};
+use std::{collections::HashSet, path::PathBuf, process::Command};
 
 #[test]
 fn same_objdump_as_ld() {
-    let to_link = [PathBuf::from("./tests/obj-files/f.o"), PathBuf::from("./tests/obj-files/f2.o")];
+    let to_link = HashSet::from([PathBuf::from("./tests/obj-files/f.o"), PathBuf::from("./tests/obj-files/f2.o")]);
     let file = NamedTempFile::new().unwrap();
 
     let options = dld::elf::Options { output: file.path().to_owned(), ..Default::default() };
-    dld::link(&to_link, &options).unwrap();
+    let linker = dld::Linker {
+        options,
+        archives: Default::default(),
+        objects: to_link.clone(),
+    };
+    linker.link().unwrap();
     let objdump_output = Command::new("objdump").arg("-d").arg(file.path()).output().unwrap();
 
     // This is assuming `ld` is GNU ld for now
